e3956c2021c8a2edc425d853b806e8bd588d1572
nitasurya <nitawijayanti966@gmail.com>
2021-11-18 22:21:43 +0700

    tes

--- application/views/Biodata_view.php
+++ application/views/Biodata_view.php
@@ -5,6 +5,9 @@
     <title>Biodata</title>
   </head>
   <body>
-    <h1>Hallo nama saya nita.</h1>
+    <h1>Hallo nama saya Nita Surya Wijayanti</h1>
+    <h1>NIM 12200387</h1>
+    <h1>Saya Kelas 12.3A.15</h1>
+    <h1>Jurusan Saya adalah Sistem Informasi</h1>
   </body>
 </html>
